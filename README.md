91a03a0a5aac7ee6b79a97a31f92339214e6fbbb
Spockuto <venkythesupersaiyan@gmail.com>
2020-10-23 00:23:44 +0100

    Fuzzer class 50%

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Fuzzing Agents
 * Local agent monitors the Service VF fuzzing.
 * Master agent monitors the workers and provides endpoint for management and control actions
 
-
+Remember
+========
+* Open socket between master and worker or use message bus?
+* initial registration + handshake?
+* need to enable some server parameters
+* Fast file monitoring suitable for continous inspection (need to check)
+* subroutine to enable core pattern
--- fuzzer.py
+++ fuzzer.py
@@ -2,6 +2,7 @@ import os
 import os.path as path
 import subprocess
 import json
+import warnings
 
 def is_installed(package):
     retval = subprocess.call(["which", package])
@@ -9,16 +10,22 @@ def is_installed(package):
         return True
     return False
 
+
 class Fuzzer :
-    def __init__(self, fuzzer, testing_dir=None, findings_dir=None, binary=None, profile=None):   
+    deploy_string = []
+    
+    def __init__(self, fuzzer, testing_dir=None, findings_dir=None, binary=None, config=None, profile=None):   
         self.set_fuzzer(fuzzer)
         self.set_testing_dir(testing_dir)
         self.set_findings_dir(findings_dir)
         self.set_binary(binary)
-        self.set_config(profile, self.fuzzer)
+        self.set_config(config, self.fuzzer)
         self.set_crashes(fuzzer)
         self.set_queue(fuzzer)
-    
+        self.set_profile(profile, self.config)
+        self.build_deploy()
+        self.build_profile()
+        
     def set_fuzzer(self, fuzzer):
         if fuzzer == "AFL" and is_installed("afl-fuzz"):
             self.fuzzer = "afl-fuzz"
@@ -55,12 +62,59 @@ class Fuzzer :
         else:
             raise OSError("Binary doesn't exist") 
     
-    def set_config(self, profile, fuzzer):
-        if not profile:
-            profile = "fuzzing.cfg"
-        if path.exists(path.abspath(profile)):
-            with open(path.abspath(profile)) as json_file:
-                config = json.load(json_file)
-                self.config = config[fuzzer]
+    def set_config(self, config, fuzzer):
+        if not config or config:
+            config = "fuzzing.cfg"
+        if path.exists(path.abspath(config)):
+            with open(path.abspath(config)) as json_file:
+                config_json = json.load(json_file)
+                self.config = config_json[fuzzer]
         else:
             OSError("Configuration file doesn't exist")
+    
+    def set_profile(self, profile, config):
+        if not profile or profile not in config["profile"]:
+            warnings.warn("Profile not found, setting Normal")
+            profile = "normal"
+        self.profile = config["profile"][profile]
+    
+    def set_args(self, args, params=None):
+        if args not in self.config["parameters"]:
+            raise ValueError("Argument not availabe")
+        Fuzzer.deploy_string.insert(-1, "-" + self.config["parameters"][args])
+        if params:
+            Fuzzer.deploy_string.insert(-1, params)
+        
+    def build_deploy(self) :
+        Fuzzer.deploy_string.append(self.config["execute"])
+        Fuzzer.deploy_string.append(self.binary)
+        self.set_args("input", self.testing_dir)
+        self.set_args("output", self.findings_dir)
+    
+    def build_profile(self):
+        if self.fuzzer == "afl-fuzz" :
+            #Need to fix core distribution logic.
+            builder = []
+            temp = Fuzzer.deploy_string
+            for core in range(int(self.profile["core"])):
+                print(temp)
+                if core == 0:
+                    self.set_args("master", "fuzzer-master")
+                else :
+                    self.set_args("slave", "fuzzer" + str(core).zfill(2))
+                builder.append(Fuzzer.deploy_string)
+                Fuzzer.deploy_string = temp
+            Fuzzer.deploy_string = builder
+            self.set_args("memory", self.profile["memory"])
+            self.set_args("timeout", self.profile["timeout"])
+        if self.fuzzer == "shellphuzz" :
+            self.set_args("core", self.profile["core"])
+            self.set_args("drill", self.profile["drill"])
+            self.set_args("memory", self.profile["memory"])
+            self.set_args("timeout", self.profile["timeout"])
+
+
+        
+
+fuzz = Fuzzer("AFL", "./dummy/in" , "./dummy/out", "./dummy/precimon_collector",'', '')
+fuzz = Fuzzer("driller", "./dummy/in" , "./dummy/out", "./dummy/precimon_collector",'', '')
